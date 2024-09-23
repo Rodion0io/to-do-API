2dab05528c49473df33a573b4a17633b093bc9af
Rodion <rybkorodion201@mail.ru>
2024-09-23 21:37:57 +0700

    add controllers and context

---
+++ to do api/Modules/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace to_do_api.Modules
+{
+	public class Card
+	{
+		public Guid Id { get; set; }
+		public string Text { get; set; }
+		public bool IsSelected { get; set; }
+
+		public Card(string Text)
+		{
+			this.Text = Text;
+            IsSelected = false;
+		}
+
+	}
+}
+
---
+++ to do api/Modules/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace to_do_api.Modules
+{
+	public class Context : DbContext
+	{
+		public DbSet<Card> Cards { get; set; }
+
+		public Context(DbContextOptions<Context> options) : base (options)
+		{
+			Database.EnsureCreated();
+
+        }
+	}
+}
+
